db5a6dee68a0c2adeb56810907de188a2a249a92
Audrianna Kelly <kellyad25@vmi.edu>
2024-09-19 10:16:06 -0400

    basic function ADDED

---
+++ AdminLogin.php
@@ -0,0 +1,39 @@
+<html>
+<style>
+div{
+    float: center;
+    vertical-align: middle;
+    background-color: #CFF1FF;
+    width: 40%;
+}
+</style>
+
+    <body background-color="#3CA1D3">
+
+            <div>
+            <form Name="userForm" method="post" action="searchUser.php">
+                <!-- <div> -->
+                <table border=0 width="0%" align="center" background-color: #CFF1FF>
+
+                    <tr>
+                        <td> Name: </td>
+                        <td> <input type="text" name="name"> </td>
+                    </tr>
+
+                    <tr>
+                        <td> Email: </td>
+                        <td> <input type="text" name="email"> </td>
+                    </tr>
+
+                    <tr>
+                        <td> </td>
+                        <td> <input type="submit" value="Search User"> </td>
+                    </tr>
+                   
+                </table>
+                <!-- </div> -->
+            </form>
+            </div>
+    </body>
+
+</html>
--- login.php
+++ login.php
@@ -1,7 +1,28 @@
-<?php
+<html>
 
-echo "Sussessful?";
+    <body>
 
-kill me please
+        <form Name="userForm" method="post" action="searchUser.php">
+        <table border=0 width="0%" align="center">
 
-?>
+            <tr>
+                <td> Name: </td>
+                <td> <input type="text" name="name"> </td>
+            </tr>
+
+            <tr>
+                <td> Email: </td>
+                <td> <input type="text" name="email"> </td>
+            </tr>
+
+            <tr>
+                <td> </td>
+                <td> <input type="submit" value="Search User"> </td>
+            </tr>
+
+        </table>
+        </form>
+
+    </body>
+
+</html>
